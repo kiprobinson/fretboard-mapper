f8afacc6641017456929868562e19ed00010ccff
Kip Robinson <kip.robinson@gmail.com>
2024-02-05 22:13:27 -0500

    add fretboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
-<script>
+<script lang="ts">
+  import { Note } from 'chord-name';
   import Controls from './controls.svelte';
+  import Fretboard from './fretboard.svelte';
+
+  let root: Note = new Note('E');
+
+  let strings: Note[] = [new Note('E'), new Note('B'), new Note('G'), new Note('D'), new Note('A'), new Note('E')];
+
+  let noteIdsInScale: Set<number> = new Set([
+    new Note('E').getId(),
+    new Note('F#').getId(),
+    new Note('G#').getId(),
+    new Note('A').getId(),
+    new Note('B').getId(),
+    new Note('C#').getId(),
+    new Note('D#').getId(),
+  ]);
 </script>
 
 <Controls />
+
+<Fretboard {strings} {noteIdsInScale} {root} />
 
 <div id="copyright">
   Fretboard Mapper created by Kip Robinson. Details at <a href="https://github.com/kiprobinson/fretboard-mapper"
---
+++ src/routes/fretboard.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+  import { Note } from 'chord-name';
+
+  export let root: Note;
+  export let noteIdsInScale: Set<number>;
+  export let strings: Note[];
+</script>
+
+<div id="fretboard">
+  {#each { length: 13 } as _, fret}
+    <div class="fret" id="fret-{fret}">
+      <div class="strings">
+        {#each strings as string}
+          {@const note = string.transpose(fret)}
+          <div class="string top">
+            {#if noteIdsInScale.has(note.getId())}
+              <div class="note" class:root={root.equals(note)} class:fifth={root.interval(note) === 7}>
+                {note.toString()}
+              </div>
+            {/if}
+          </div>
+          <div class="string bottom"></div>
+        {/each}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  #fretboard {
+    font-size: 0.8rem;
+    display: grid;
+    grid-template-columns: 1fr repeat(12, 2fr);
+    gap: 1px;
+    width: 7in;
+    height: 1.5in;
+    margin: 0.25in 0;
+  }
+
+  .fret {
+    width: calc(100% + 2px);
+    height: calc(100% + 2px);
+  }
+
+  .strings {
+    display: grid;
+    width: 100%;
+    height: 100%;
+  }
+  .string {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    position: relative;
+  }
+  #fret-0 .string {
+    border-left: none;
+    border-bottom: none;
+    border-right-width: 3px;
+  }
+  .string.top {
+    border-bottom: 1px solid black;
+  }
+
+  .strings .string:first-child,
+  .strings .string:last-child {
+    border-left: none;
+    /* note to self- can't do border-right: none because it messes up alignment. */
+    border-right-color: rgba(255, 255, 255, 0);
+  }
+
+  .note {
+    position: absolute;
+    top: 0.25em;
+    right: 1px;
+    width: 1.5em;
+    height: 1.5em;
+    border: 1px solid black;
+    border-radius: 0.75em;
+    text-align: center;
+    padding-top: 1px;
+    color: #666;
+    background-color: white;
+  }
+  .note.root {
+    color: black;
+    font-weight: bold;
+    border-width: 2px;
+  }
+  .note.fifth {
+    color: black;
+    font-weight: bold;
+  }
+
+  #fret-3,
+  #fret-5,
+  #fret-7,
+  #fret-9 {
+    background-image: url('fretboard-dots-1.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  #fret-12 {
+    background-image: url('fretboard-dots-2.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+</style>
